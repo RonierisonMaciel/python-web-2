093e3894a3b32627a2179d4296db5178f3d18aa7
ronierisonmaciel <ronierison.maciel@gmail.com>
2022-11-07 17:24:24 -0300

    feat(fix): :zap: fix
    fix

--- swapi/main.py
+++ swapi/main.py
@@ -1,6 +1,20 @@
+import os
+
 from fastapi import FastAPI
+from sqlmodel import Session
+from .db import create_db_and_tables, engine, populate_all_table
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    if not os.path.exists("database.sqlite"):
+        create_db_and_tables()
+        with Session(engine) as session:
+            populate_all_table(session)
+    else:
+        create_db_and_tables
 
 
 @app.get("/")
