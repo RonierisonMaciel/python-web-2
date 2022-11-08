f01d57058afcd33e17a0698c94d2a4365b5f1e02
ronierisonmaciel <ronierison.maciel@gmail.com>
2022-11-08 09:36:05 -0300

    feat(fix): :fire:
    fix

--- swapi/main.py
+++ swapi/main.py
@@ -1,8 +1,10 @@
 import os
 
 from fastapi import FastAPI
-from sqlmodel import Session
-from .db import create_db_and_tables, engine, populate_all_table
+from sqlmodel import Session, select
+from .db import create_db_and_tables, engine, populate_all_tables
+
+from swapi.model import Planet
 
 app = FastAPI()
 
@@ -12,11 +14,23 @@ def on_startup():
     if not os.path.exists("database.sqlite"):
         create_db_and_tables()
         with Session(engine) as session:
-            populate_all_table(session)
+            populate_all_tables(session)
     else:
         create_db_and_tables
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+def create_response(result):
+    return {
+        "count": len(result),
+        "next": None,
+        "previous": None,
+        "results": result,
+    }
+
+
+@app.get("/api/planets/")
+async def list_planets():
+    with Session(engine) as session:
+        planets = session.exec(select(Planet)).all()
+
+    return create_response(planets)
